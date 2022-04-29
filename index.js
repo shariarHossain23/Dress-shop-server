4d8b9ac5cdfba375ee6dd64a449afe83ff2b7175
shariarhossain23e <shariarhossain23@gamil.comm>
2022-04-30 00:58:10 +0600

    end backend

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hello assignment 11");
-}) /
+}) 
   app.listen(port, () => {
     console.log("assigment 11 runnig");
   });
